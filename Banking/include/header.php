e8cb8b956f13e088b7a7efc39c3dc0cbf86cf7cf
Shweta jha <85930909+shwetaj954@users.noreply.github.com>
2021-07-04 22:40:32 -0700

    Add files via upload

---
+++ Banking/include/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity=
+            "sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous" />
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+			<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css" integrity="sha384-wESLQ85D6gbsF459vf1CiZ2+rr+CsxRY0RpiF1tLlQpDnAgg6rwdsUF1+Ics2bni" crossorigin="anonymous">
+ 
+            <title>Basic Bank</title>
+           <style>
+                
+          
+           </style>
+        </head>
+    <body class="bgt"  onload="Captcha();">
+    <nav class="navbar navbar-expand-lg bg-primary navbar-light border-top border-bottom">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#"><img src="assets/img/logo2.png" height="50"></a>
+                <a class="navbar-brand text-light" href="index.php"><b>Home</b><i class="material-icons align-middle">home</i></a>
+            </div>
+        </nav>
+    </body>
+</html>
