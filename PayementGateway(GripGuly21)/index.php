0a859887c2d3d568adea8f08a4d8a2f14806108c
Shweta jha <85930909+shwetaj954@users.noreply.github.com>
2021-07-20 09:02:13 -0700

    Create index.php

---
+++ PayementGateway(GripGuly21)/index.php
@@ -0,0 +1,46 @@
+<?php
+require_once 'header.php';
+?>
+<div class="container" style="height: 100vh;">
+<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://www.familyeducation.com/sites/default/files/2019-02/why-independent-play-is-vital-for-raising-empowered-kids.jpg" class="d-block w-100" alt="..." style="height: 100vh;">
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Welcome To My Page</h5>
+        <p>Our Aim To Save the Child</p>
+        <a href="donate.php" class="btn donate-button ">Donate Now</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://il3.picdn.net/shutterstock/videos/2390780/thumb/1.jpg" class="d-block w-100" alt="..." style="height: 100vh;">
+      <div class="carousel-caption d-none d-md-block">
+      <h5>Welcome To My Page</h5>
+        <p>Our Aim To Save the Child</p>
+        <a href="donate.php" class="btn donate-button ">Donate Now</a>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="https://the.ismaili/sites/default/files/5._childrens_day_2.jpg" class="d-block w-100" alt="..." style="height: 100vh;">
+      <div class="carousel-caption d-none d-md-block">
+      <h5>Welcome To My Page</h5>
+        <p>Our Aim To Save the Child</p>
+        <a href="donate.php" class="btn donate-button">Donate Now</a>
+      </div>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
